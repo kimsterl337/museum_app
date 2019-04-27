2b28d982c2de6e75b7e1185a834eeb58b7a6f6fd
Thomas Kim <thomas.keunback.kim@gmail.com>
2019-04-27 14:39:27 -0400

    Updates

---
+++ precache-manifest.395e45e62f00c8a203dfcf510dd78108.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50a5b4573340fcc39a2cc4e56bb250d9",
+    "url": "/museum_app/index.html"
+  },
+  {
+    "revision": "2442a2e63bf53dc1f863",
+    "url": "/museum_app/static/css/main.32a2050a.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/museum_app/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "2442a2e63bf53dc1f863",
+    "url": "/museum_app/static/js/main.9c7887f0.chunk.js"
+  },
+  {
+    "revision": "a6da1ac22736af98864b",
+    "url": "/museum_app/static/js/runtime~main.148c63ef.js"
+  }
+]);
